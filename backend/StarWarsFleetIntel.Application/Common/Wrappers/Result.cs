262a5f559d7bab3b0c6dfbb6cda880fd3824f6e6
Mohamed Aboseif <midozenelelime@gmail.com>
2025-10-23 18:46:17 +0300

    feat(backend/Wrappers): add result pattern
    - Add Result<T>
    - Add PaginatedResult<T>

---
+++ backend/StarWarsFleetIntel.Application/Common/Wrappers/Result.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarWarsFleetIntel.Application.Common.Wrappers
+{
+    public class Result<T>
+    {
+        public bool Succeeded { get; set; }
+        public string Message { get; set; } = string.Empty;
+        public List<string> Errors { get; set; } = new();
+        public T? Data { get; set; }
+
+        public static Result<T> Success(T data, string message = "")
+        {
+            return new Result<T>
+            {
+                Succeeded = true,
+                Data = data,
+                Message = message
+            };
+        }
+
+        public static Result<T> Failure(string message, List<string>? errors = null)
+        {
+            return new Result<T>
+            {
+                Succeeded = false,
+                Message = message,
+                Errors = errors ?? new List<string>()
+            };
+        }
+    }
+}
